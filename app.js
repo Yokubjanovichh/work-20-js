25f7ddbc3f6abc06f6f9fd6a29aaba9ded17fc38
Murodulla <yokubjanovich@gmail.com>
2022-06-25 14:58:14 +0500

    dispritatsiya

--- app.js
+++ app.js
@@ -7,20 +7,34 @@ let thief = {
     hair: "yellow",
     style: "curly",
   },
+  howOut: function(){
+    console.log("by window");
+  }
 };
+
+thief.howOut();
+
+const {hair, style} = thief.colors;
+
+console.log(hair, style);
 
 // delete thief.jacket;
 // console.log(thief);
 
 // console.log(Object.keys(thief));
-console.log(Object.keys(thief).length); // obyekt uzinliginio kursatadi;
+// console.log(Object.keys(thief).length); // obyekt uzinliginio kursatadi;
 
-for (let key in thief) {
-  if (thief[key] === "Object") {
-    for (let i in thief[key]) {
-      console.log(`Property ${i} has been ${thief[key][i]}`);
-    }
-  } else {
-    console.log(`Property ${key} has been ${thief[key]}`);
-  }
-}
+// let count = 0;
+
+// for (let key in thief) {
+//   count++;
+//   if (thief[key] === "Object") {
+//     for (let i in thief[key]) {
+//       console.log(`Property ${i} has been ${thief[key][i]}`);
+//     }
+//   } else {
+//     console.log(`Property ${key} has been ${thief[key]}`);
+//   }
+// }
+
+// console.log(count);
